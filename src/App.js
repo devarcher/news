377100957ad817610f947e352dcd5984daaafee3
devarcher <archershot@hotmail.com>
2020-01-16 17:29:58 -0600

    found correct uris for hackernews

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-
+import React from 'react'
 import "./App.css";
 
-function App() {
+const App = () => {
+
+// Hacker News: Relevant URIs.
+const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
+const newStoriesUrl = `${baseUrl}newstories.json`;
+const storyUrl = `${baseUrl}items/`
+
+
+const getStoryIds = async () => {
+  const response = await fetch(newStoriesUrl); 
+  const data = await response.json()
+  return data
+}
+
   return (
     <div className="App">
-      
+      <p></p>
     </div>
   );
 }
