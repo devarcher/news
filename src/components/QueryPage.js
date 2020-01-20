5e1b8e3ede50f03c84e91b3369d00422a4b56661
devarcher <archershot@hotmail.com>
2020-01-19 22:03:39 -0600

    styled results

--- src/components/QueryPage.js
+++ src/components/QueryPage.js
@@ -10,6 +10,7 @@ const QueryPage = props => {
   return (
     <div className="container">
       <div className="searchContainer">
+        <h1>Hacker News</h1>
         <select className="selectorBox" onChange={e => selectHandler(e)}>
           <option value="general">General Search</option>
           <option value="author">Search By Author</option>
@@ -39,13 +40,13 @@ const QueryPage = props => {
             <li className="articleLi" key={article.id}>
               <div className="titleAuthor">
                  {article.link ? <a href={article.link} target="blank_"><h1>{article.title}</h1></a> : <h1>{article.title} -- No Link</h1>}
-                <h3>Author: {article.author}</h3>
+                <h2>Author: {article.author}</h2>
               </div>
              
               <div>
-                <h4>Points: {article.points}</h4> 
-                <h4>Date:
-                {moment(article.date_created).format("MM/DD/YYYY")}</h4>
+                <h3>Points: {article.points}</h3> 
+                <h3>Date:
+                  {moment(article.date_created).format("MM/DD/YYYY")}</h3>
               </div>
             </li>
           ))}
